f74d155701475f683d63c24307c4c65fabe032fb
Tahaabdul <rhaxaqi@gmail.com>
2022-12-08 18:02:28 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  main - Print a string
+ *
+ *  Description: use printf to print out
+ *
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
