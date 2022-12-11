3a8900a2690e03a67f6cd67279d34ffc1c53532a
Tahaabdul <rhaxaqi@gmail.com>
2022-12-11 05:49:07 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - print all single dits base10 numbers
+ * using putchar only
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for(n = 48; n < 58;  n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
