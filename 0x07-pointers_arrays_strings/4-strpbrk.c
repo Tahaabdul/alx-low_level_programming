20218b6b3797ef1174817bc800f3fb5a2c870427
Tahaabdul <rhaxaqi@gmail.com>
2023-01-09 09:31:07 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - finds first occurence of bytes from accept in string
+ * @s: string to locate occurence of accept in
+ * @accept: the array to use for search
+ * Return: returns bytes found or NULL.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		} 
+	}
+	return (0);
+}
