2fe33dc84bc0a2be044b193265c0dc3f1fb8f716
Tahaabdul <rhaxaqi@gmail.com>
2023-01-16 13:33:15 -0800

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+#REPOSITORIES FOR code under 0x0E. C - Structures, typedef tasks in ALx
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+
+
+/**
+ * struct dog - creates a struct "dog"
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
