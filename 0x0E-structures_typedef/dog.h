380651163d43b571951b853e6680f79b371a049e
Tahaabdul <rhaxaqi@gmail.com>
2023-01-17 18:57:48 -0800

    Task 4 & 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,7 +18,11 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-typedef struct dog new_dog;
 typedef struct dog dog_t;
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
