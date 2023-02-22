c083363fe9f384922456d9f3134ac9e1547f0f61
Tahaabdul <rhaxaqi@gmail.com>
2023-02-21 16:12:16 -0800

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalphs -  a function to if character is aphabet
+ * @c: character to check
+ * Return: 1 for true 0 for false
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
