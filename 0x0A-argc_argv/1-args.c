c2d952a65d395eb0d71c665f229cd8b22c1911fa
Tahaabdul <rhaxaqi@gmail.com>
2023-06-14 17:04:05 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: count o:f arguments
+ * @argv: array of arguments values
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
