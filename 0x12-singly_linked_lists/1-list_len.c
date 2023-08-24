8ab0064689bc479bb4cf9a08beec047b32bc9ad9
tahaabdul <rhaxaqi@gmail.com>
2023-08-24 16:38:33 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - Calculate the total count of nodes in a linked list.
+ * This function calculates the total count of nodes in a given linked list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: Total count of nodes in the linked list.
+ */
+
+
+size_t list_len(const list_t *h)
+{
+	size_t total_count = 0;
+
+	while (h != NULL)
+	{
+		total_count += h->len;
+		h = h->next;
+	}
+	return (total_count);
+}
