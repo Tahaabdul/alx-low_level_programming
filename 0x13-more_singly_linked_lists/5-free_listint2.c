be28b8ace2d59bbbf28556a8f248e434abb7b42d
tahaabdul <rhaxaqi@gmail.com>
2023-08-30 00:15:17 -0700

    Revert "Task 5 correction"
    This reverts commit 59b542f77bfac60139ff7cc0e59aedf4947139b2.

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return;
-
 	listint_t *current = *head;
 	listint_t *next_node;
+
+	if (head == NULL || *head == NULL)
+		return;
 
 	while (current != NULL)
 	{
