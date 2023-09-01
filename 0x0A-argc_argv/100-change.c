b0d7cfcd84602a1a0ec7eac415b832ac716d9365
Tahaabdul <rhaxaqi@gmail.com>
2023-09-01 16:42:34 -0700

    Advanced task 1

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,50 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-
-/**
- * main - Calculates minimum coins for making change
- * @argc: count of argument
- * @argv: vector of arguments
- * Return: return number of coins to use in a change
- */
-
-int main(int argc, char *argv[])
-{
-	int cents;
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int count = 0;
-	int i;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 0; i < num_coins; i++)
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			count++;
-		}
-	}
-
-	printf("%d\n", count);
-
-	return (0);
-}
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Calculates minimum coins for making change
