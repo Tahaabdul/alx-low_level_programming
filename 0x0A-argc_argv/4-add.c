6cc857404525b149171e069ca7cd3eef291d9813
Tahaabdul <rhaxaqi@gmail.com>
2023-09-01 16:43:43 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,41 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <limits.h>
 
-/**
- * main - prints the sum of args
- * @argc: count of argument
- * @argv: vector of arguments
- * Return:0 for no number, not digits print"Error" return 1
- */
+int main(int argc, char *argv[]) {
+    int i;
+    int sum = 0;
 
-int main(int argc, char *argv[])
-{
-	int i;
-	int sum = 0;
+    if (argc > 1) {
+        for (i = 1; i < argc; i++) {
+            char *endptr;
+            long num = strtol(argv[i], &endptr, 10);
 
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (atoi(argv[i]) != 0)
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", sum);
-	}
-	else
-	{
-		printf("%d\n", 0);
-	}
+            if (*endptr == '\0' && num > 0 && num <= INT_MAX) {
+                sum += num;
+            } else {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        printf("%d\n", sum);
+    } else {
+        printf("%d\n", 0);
+    }
 
-	return (0);
+    return 0;
 }
-
-
