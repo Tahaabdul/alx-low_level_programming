95d552ef614a07772b18be328b22f823b97e830b
Tahaabdul <rhaxaqi@gmail.com>
2023-10-02 18:35:58 -0700

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+/**
+ * read_textfile - Reads and prints the content of a text file.
+ * @filename: The name of the text file to read.
+ * @letters: The number of letters (bytes) to read and print.
+ * Return: the actual number of letters read and printed, or 0 on failure.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buffer = malloc(letters);
+        ssize_t bytes_read = read(fd, buffer, letters);
+        ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (filename == NULL)
+		return (0);
+
+	if (fd == -1)
+		return (0);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
